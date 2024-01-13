6377eeadb915066d7626f5281add9e97e8ac59bf
Bibek Kunwar <kunwarbibek9233@gmail.com>
2024-01-13 09:46:45 +0545

    Logout user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,10 @@
 import { Router } from "express";
+import { loginUser } from "../controllers/user.controllers.js";
+import { logoutUser } from "../controllers/user.controllers.js";
 import { registerUser } from "../controllers/user.controllers.js";
 //Normally we can handle data only so if we handle file we need to upload from multer 
 import {upload} from "../middlewares/multer.middleware.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router()
 
 router.route("/register").post(
@@ -17,4 +20,8 @@ router.route("/register").post(
     ]),
     registerUser
     )
+    router.route("/login").post(loginUser)
+
+    //secured routes
+    router.route("/logout").post(verifyJWT,  logoutUser)
 export default router
